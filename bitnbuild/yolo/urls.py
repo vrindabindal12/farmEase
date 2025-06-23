1a7be79dcad25c5790510f3da65237a994b8f58f
vrindabindal12 <vrindabindal1212@gmail.com>
2025-06-23 13:01:27 +0530

    viwes-py file

---
+++ bitnbuild/yolo/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import out_of_stock
+
+urlpatterns = [
+    path('out_of_stock', out_of_stock, name='out_of_stock'),
+]
--- bitnbuild/yolo/views.py
+++ bitnbuild/yolo/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from ultralytics import YOLO
+import cv2
+from inventory.models import Product
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def out_of_stock(request):
+    if request.method == 'GET':
+        model = YOLO('yolov8n.pt')
+        banana_product = get_object_or_404(Product, name="Banana")
+        if banana_product.quantity_remaining > 0:
+            banana_product.quantity_remaining -= 1
+            qty = banana_product.quantity_remaining
+            banana_product.save()
+            message = "Banana quantity decreased successfully."
+        else:
+            message = "No bananas left in stock."
+        print(message)
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = cap.read()
+            results = model(frame, show=True)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+        return render(request, 'out_of_stock.html')
+    else:
+        return render(request, 'out_of_stock.html')
